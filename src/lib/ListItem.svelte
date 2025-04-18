4457c3fa1f103e35295231237df722da45bef96f
yuzuyyy <hendriawan0720@gmail.com>
2025-04-18 13:43:51 +0800

    commit

--- src/lib/ListItem.svelte
+++ src/lib/ListItem.svelte
@@ -1,10 +1,10 @@
 <script>
   import { arrow, shuriken } from "../assets";
-    export let image = arrow
-    export let text = "text"
+  export let image = arrow;
+  export let text = "text";
 </script>
 
 <div class="flex items-center gap-[8px]">
-    <img class="size-[24px]" src={image} alt="">
-    <p class="body-1 text-white/80 capitalize">{text}</p>
+  <img class={image === arrow ? "size-[16px]" : "size-[24px]"} src={image} alt="" />
+  <p class="body-1 text-white/80 capitalize">{text}</p>
 </div>
