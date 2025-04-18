b1660e9bff15b7d29b480aa238a900f1b4afe606
yuzuyyy <hendriawan0720@gmail.com>
2025-04-18 16:07:02 +0800

    commit

--- src/lib/ListItem.svelte
+++ src/lib/ListItem.svelte
@@ -5,6 +5,10 @@
 </script>
 
 <div class="flex items-center gap-[8px]">
-  <img class={image === arrow ? "size-[16px]" : "size-[24px]"} src={image} alt="" />
+  <img
+    class={image === arrow ? "size-[16px]" : "size-[24px]"}
+    src={image}
+    alt=""
+  />
   <p class="body-1 text-white/80 capitalize">{text}</p>
 </div>
