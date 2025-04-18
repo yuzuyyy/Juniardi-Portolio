20c29b5f87dbbbfed0b46fe4e56b853c5e375ab6
yuzuyyy <hendriawan0720@gmail.com>
2025-04-18 09:20:59 +0800

    commit

--- src/lib/About.svelte
+++ src/lib/About.svelte
@@ -17,13 +17,13 @@ import {ListItem, Number} from "./";
         <!-- about me start -->
         <div class="grid grid-cols-2 max-lg:grid-cols-1 gap-[24px]">
             <div class="flex flex-col justify-between lg:min-h-[280px] h-fit pt-[16px] gap-[10px]">
-                <p class="body-1 text-white/80">Design, for me, goes beyond solving problems. It's about improving user experiences, striking the perfect balance between aesthetics and functionality, and even creating products that form emotional connections with users.</p>
+                <p class="body-1 text-white/80 md:max-lg:max-w-[480px]">Design, for me, goes beyond solving problems. It's about improving user experiences, striking the perfect balance between aesthetics and functionality, and even creating products that form emotional connections with users.</p>
 
                 <p class="heading-5 text-white">Signature</p>
             </div>
 
             <div class="flex flex-col items-start pl-[80px] max-lg:p-0 gap-[24px]">
-                <h2 class="heading-2 text-white capitalize">My <span class="text-primary">services</span></h2>
+                <h2 class="heading-3c text-white capitalize">My <span class="text-primary">services</span></h2>
                 {#each services as service } 
                 <ListItem text={service}/>
                 {/each}
