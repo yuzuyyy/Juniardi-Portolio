272810e0b7c3fa2a941385006e1e573bc4a47eef
yuzuyyy <hendriawan0720@gmail.com>
2025-04-18 08:31:39 +0800

    commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-import { socmed1 } from "../assets";
+import { socmed1, socmed2, socmed3, socmed4 } from "../assets";
 
 export const socmeds = [
     {
@@ -6,15 +6,15 @@ export const socmeds = [
         link: "https://twitter.com"
     },
     {
-        icon: socmed1,
+        icon: socmed2,
         link: "https://twitter.com"
     },
     {
-        icon: socmed1,
-        link: "https://twitter.com"
+        icon: socmed3,
+        link: "https://dribbble.com"
     },
     {
-        icon: socmed1,
+        icon: socmed4,
         link: "https://twitter.com"
     },
 ]
