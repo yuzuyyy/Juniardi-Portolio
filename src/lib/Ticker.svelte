dda7ab43d44c78609c56cd689e6cd144f5c8d6e5
yuzuyyy <hendriawan0720@gmail.com>
2025-04-18 13:54:40 +0800

    commit

--- src/lib/Ticker.svelte
+++ src/lib/Ticker.svelte
@@ -21,7 +21,7 @@
           >
             <!-- Quadrupled content (4 identical components) -->
             {#each Array(4) as _}
-              <div class="flex items-center gap-4 mx-8">
+              <div class="flex items-center gap-4 mx-16">
                 <img src={arrow} alt="shuriken" class="w-10 h-10 max-md:w-6 max-md:h-6" />
                 <p class="text-[40px] max-md:text-[24px]  text-white">Featured work</p>
               </div>
