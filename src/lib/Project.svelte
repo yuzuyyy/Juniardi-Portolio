e5e59bec674ce9681382ed612e57ced6b766a2da
yuzuyyy <hendriawan0720@gmail.com>
2025-04-19 10:42:53 +0800

    commit

--- src/lib/Project.svelte
+++ src/lib/Project.svelte
@@ -1,20 +1,23 @@
 <script>
   import { MainButton, ProjectCard } from ".";
-    import { projects } from "../constants";
+  import { projects } from "../constants";
+  import FadeIn from "./FadeIn.svelte"; // import FadeIn
 </script>
 
 <section id="project" class="bg-white/80 w-full section-padding">
   <div class="w-full flex flex-col gap-[40px] max-md:gap-[24px] section-container">
+
     <div>
       <MainButton className="w-fit button-text ">Become a client</MainButton>
     </div>
 
-    <div class=" grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px] w-full">
-      {#each projects as project  }
-        <ProjectCard {...project} />
+    <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-[24px] w-full">
+      {#each projects as project, i}
+        <FadeIn delay={i * 150} wait={300}>
+          <ProjectCard {...project} />
+        </FadeIn>
       {/each}
     </div>
 
-   
   </div>
 </section>
